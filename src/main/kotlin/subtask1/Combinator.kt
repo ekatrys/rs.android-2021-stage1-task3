e9d6f1dbe7b4787aeaee4f97d693a219bdcbfc32
Ekaterina <ekatrys@gmail.com>
2021-07-02 19:27:51 +0200

    add realization 1 and 3 tasks

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -4,6 +4,22 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val numPosters = array[0]
+        val numColor = array[1]
+        var numChoose: Int? = null
+
+        for (i in 1..numColor) {
+            if (cOtNpoM(numPosters, numColor, i)) {
+                numChoose = i
+                break
+            }
+        }
+        return numChoose
+    }
+
+    private fun factorial(n: Int): Long = if (n < 2) 1 else n * factorial(n - 1)
+
+    private fun cOtNpoM(c: Int, n: Int, m: Int): Boolean {
+        return c.toLong() == factorial(n) / (factorial(m) * factorial(n - m))
     }
 }
