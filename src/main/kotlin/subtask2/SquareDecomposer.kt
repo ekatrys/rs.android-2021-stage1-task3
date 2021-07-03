a89213ac804bba65fe43abe6f79698008dce4af7
Ekaterina <ekatrys@gmail.com>
2021-07-03 15:05:31 +0200

    add realization 2 tasks

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -4,6 +4,30 @@ class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        val newNumber = number.toLong()
+        val result = mutableListOf(newNumber)
+        var remain = 0L
+
+        while (!result.isNullOrEmpty()) {
+            val current = result.last()
+            result.indexOfLast {
+                result.remove(it)
+            }
+
+            remain += square(current)
+            for (i in current-1 downTo 0) {
+                if (remain - square(i) >= 0){
+                    remain -= square(i)
+                    result.add(i)
+
+                    if (remain == 0L){
+                        result.sort()
+                        return result.map{it.toInt()}.toTypedArray()
+                    }
+                }
+            }
+        }
+        return null
     }
+    fun square(x: Long) = x * x
 }
--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -1,20 +1,39 @@
 package subtask3
 
-import java.io.File
-
 class TelephoneFinder {
 
     // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
         val num = number.toIntOrNull()
         if (num == null || num < 0) return null
+        val newList = mutableListOf<String>()
         val numInt = number
             .split("")
             .toMutableList()
+
         numInt.removeAll { it == "" }
 
-        val newList = mutableListOf<String>()
+        numInt.forEachIndexed { index, it ->
+            changeAllNaiborChar(number, index, naibor[it]!!, newList)
+        }
+        return newList.toTypedArray()
+    }
 
+   private fun changeAllNaiborChar(
+        number: String,
+        index: Int,
+        array: Array<String>,
+        newArray: MutableList<String>
+    ) {
+        for (element in array) {
+            newArray.add(changeChar(number, index, element))
+        }
+    }
+
+    private fun changeChar(number: String, index: Int, charToReplace: String) =
+        number.substring(0, index) + charToReplace + number.substring(index + 1)
+
+    companion object {
         val naibor = mapOf(
             "1" to arrayOf("2", "4"),
             "2" to arrayOf("1", "3", "5"),
@@ -27,26 +46,5 @@ class TelephoneFinder {
             "9" to arrayOf("6", "8"),
             "0" to arrayOf("8")
         )
-
-
-        numInt.forEachIndexed { index, it ->
-            changeAllNaiborChar(number, index, naibor[it]!!, newList)
-        }
-        return newList.toTypedArray()
     }
 }
-
-
-fun changeAllNaiborChar(
-    number: String,
-    index: Int,
-    array: Array<String>,
-    newArray: MutableList<String>
-) {
-    for (element in array) {
-        newArray.add(changeChar(number, index, element))
-    }
-}
-
-fun changeChar(number: String, index: Int, charToReplace: String) =
-    number.substring(0, index) + charToReplace + number.substring(index + 1)
